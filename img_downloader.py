2678acd8526fb3b6b6a08acaa8981614595ece3c
George Liu <alice@localhost.localdomain>
2023-02-19 21:10:11 +0800

    complete the basic download function

---
+++ img_downloader.py
@@ -0,0 +1,31 @@
+"""photo downloader
+the name of the downloaded photo will be time like this formate:21:02:47
+change the number in the parenthesis after manypic 
+so that you can download imgs as many as you want
+"""
+import requests
+from datetime import datetime
+
+url = 'https://api.vvhan.com/api/view'
+#public api to invoke
+
+def filename():
+	#use strftime as filename
+	current_time = datetime.now().strftime("%X")
+	return current_time
+
+def downloader(url,filename):
+	#function to download img
+	r = requests.get(url)
+	with open(f'./{filename}.png','wb') as f:
+		f.write(r.content)    #wrtite into the file
+	print(f'{filename}.png downloaded!\n')   #success info
+
+def manypic(number):
+	#download as many img as you want 
+	for i in range(number):
+		downloader(url,filename())
+
+manypic(5)
+
+
